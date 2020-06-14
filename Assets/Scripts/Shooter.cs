615027d529dec8a50399f27636af00205140fe51
AdityaDas-IITM <me19b194@smail.iitm.ac.in>
2020-06-14 17:37:02 +0530

    first commit

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    
+    AreaScript areaScript;
+    // Update is called once per frame
+    public void shoot()
+    {
+        
+        areaScript =transform.parent.parent.GetComponentInParent<AreaScript>();
+        
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 15))
+        {
+            GameObject shot = hit.transform.gameObject;
+            if(shot.CompareTag("enemy"))
+            {
+                areaScript.DestroyEnemy(shot);
+                Debug.Log("Hit Enemy");
+            }
+
+        }
+        
+    }
+}
